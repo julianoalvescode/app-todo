5f28c9abcdee04668e3e169f52ab5686fd089cba
Juliano Alves <contato.julianoalvess@gmail.com>
2021-07-28 08:07:16 -0300

    feat: 🎸 release 0.1
    release 0.1
    
    BREAKING CHANGE: 🧨 release 0.1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,38 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from "../components/Header";
+import { Task, TasksList } from "../components/TasksList";
+import { TodoInput } from "../components/TodoInput";
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    if (newTaskTitle) {
+      setTasks((oldState) => [
+        ...oldState,
+        {
+          title: newTaskTitle,
+          id: Math.floor(Math.random() * 1000),
+          done: false,
+        },
+      ]);
+    }
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const newTasks = tasks.map((item) =>
+      item.id === id ? { ...item, done: !item.done } : item
+    );
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const newTasks = tasks.filter((item) => item.id !== id);
+
+    setTasks(newTasks);
   }
 
   return (
@@ -26,18 +41,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: "#EBEBEB",
+  },
+});
